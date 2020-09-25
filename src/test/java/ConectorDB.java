1c173c1cee0d2468b65120bab3bea98afc8d9eaf
james <james@HP-Ubuntu>
2020-09-25 10:55:12 -0400

    DB

--- src/test/java/ConectorDB.java
+++ src/test/java/ConectorDB.java
@@ -3,42 +3,42 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import javax.swing.JOptionPane;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author james
  */
 public class ConectorDB {
     
-        public static final String USERNAME="root";
+    public static final String USERNAME="root";
     public static final String PASSWORD="Guatemala13.";
     //public static final String URL="jdbc:mysql://localhost:3306/hospital";
-    public static final String URL="jdbc:mysql://localhost:3306/hospital?useTimezona=true&serverTimezone=CST";
-     
+    public static final String URL="jdbc:mysql://localhost:3306/tienda?useTimezona=true&serverTimezone=CST";
+    public static Connection con;
     
 
     public static Connection getConnection(){
-    Connection con=null;
     try{
         
     Class.forName("com.mysql.cj.jdbc.Driver");
     
     con =(Connection) DriverManager.getConnection(URL,USERNAME,PASSWORD);
-    //JOptionPane.showMessageDialog(null,"Conexion exitosa");
-    
+
     } catch(Exception e){
-    
-    
-    
+
     }
     return con;   
-        
-    
     }
+    
+    
+    public static void cerrarConector(){
+        try{
+            if(con != null)
+            con.close();
+        } catch (Exception e){
+        System.out.println("Error: No se logro cerrar conexion:\n"+e);
+    }
+    }
+    
+    
     
 }
